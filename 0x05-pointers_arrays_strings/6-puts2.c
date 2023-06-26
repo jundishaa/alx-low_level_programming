b46a9d2ce453e56eed801c776c276043b806f185
Jundi <jundiyusuf10@gmail.com>
2023-06-26 14:16:14 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *puts2 - prints every other character
+  *@str: the string to be printed
+  *Return: 0
+  */
+void puts2(char *str)
+{
+	int string;
+
+	for (string = 0; str[string] != '\0'; string++)
+		if (string % 2 == 0)
+			_putchar(str[string]);
+	_putchar('\n');
+}
