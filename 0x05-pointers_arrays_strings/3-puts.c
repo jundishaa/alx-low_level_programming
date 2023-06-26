a2c69331cdb4778842dc3c87fd1ca2cc40d07fc7
Jundi <jundiyusuf10@gmail.com>
2023-06-26 09:46:33 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *_puts - prints strings
+  *@str: string
+  *Return: 0
+  */
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
