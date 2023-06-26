449e3b79c3090c70bb3112cb74ff2ae254539896
Jundi <jundiyusuf10@gmail.com>
2023-06-26 15:27:55 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+  *_atoi - convert string to an enteger
+  *@s: the string to be printed
+  *Return: 0
+  */
+int _atoi(char *s)
+{
+	int a = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
+
+	while (s[a])
+	{
+		if (s[a] == 45)
+		{
+			min *= -1;
+		}
+		while (s[a] >= 48 && s[a] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[a] - '0');
+				a++;
+		}
+		if (isi == 1)
+		{
+			break;
+		}
+		a++;
+	}
+	ni *= min;
+	return (ni);
+}
