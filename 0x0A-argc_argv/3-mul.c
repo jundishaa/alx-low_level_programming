7a34269d2713bb72d68e61d8ddf8b2eb6fd572b6
Jundi yusuf <jundiyusuf10@gmail.com>
2023-11-17 15:19:45 -0800

    argc

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - multiply two numbers
+  *@argc:argument counts
+  *@argv: argument
+  *Return: 0
+  */
+int main(int argc, char **argv)
+{
+	int i, j;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	printf("%d\n", i * j);
+	return (0);
+}
+
